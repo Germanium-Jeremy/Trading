c1d5df829aef5b2621ce92aaca67cefbbd160c86
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2024-06-19 16:24:18 +0200

    initial

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
-const express = require("express")
-require("dotenv").config()
-const app = express()
+const express = require("express");
+require("dotenv").config();
+const app = express();
 
-app.get("/", (req, res) => res.status(200).json("Index Route"))
+app.use(express.json());
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
+app.get('/', (req, res) => res.send("Index Route"));
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
