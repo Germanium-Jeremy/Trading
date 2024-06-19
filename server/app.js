c02a09a33d63eda48e781942b94acf2c74244fb0
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2024-06-19 16:50:06 +0200

    ini

--- server/app.js
+++ server/app.js
@@ -13,9 +13,12 @@ app.use(
 );
 
 app.get("/", (req, res) => res.send("Index Route"));
-app.get("/getting", (req, res) => res.send("Getting Routes"));
+app.get("/getting", (req, res) => {
+     res.send("Getting Routes")
+});
 
-app.listen(process.env.PORT, (req, res) => {
-     console.log(`Server running on port ${process.env.PORT}`)
-     return res.send("Index Routing Home")
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+     console.log(`Server running on port ${port}`)
 });
