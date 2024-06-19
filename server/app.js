d5435f0a54bf86d185810ba97b0b8332ff11745a
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2024-06-19 16:25:54 +0200

    initia

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => res.send("Index Route"));
+app.get('/getting', (req, res) => res.send("Getting Routes"));
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
