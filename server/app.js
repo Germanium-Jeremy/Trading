4e62a97c9d48109bf05cd7eb099bce68e57e9606
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2024-06-19 16:39:25 +0200

    initi

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (req, res) => res.send("Index Route"));
-app.get('/getting', (req, res) => res.send("Getting Routes"));
-
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
 );
+
+app.get("/", (req, res) => res.send("Index Route"));
+app.get("/getting", (req, res) => res.send("Getting Routes"));
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
