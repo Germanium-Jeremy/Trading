67bf65896e6499df7b28d1810ff2307888f5d0df
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2024-06-19 17:05:28 +0200

    i

--- server/app.js
+++ server/app.js
@@ -6,19 +6,17 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: ["*", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
 app.get("/", (req, res) => res.send("Index Route"));
-app.get("/getting", (req, res) => {
-     res.send("Getting Routes")
+app.get("/", (req, res) => {
+     res.send("Index route for our chat app ..");
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 15000;
 
-app.listen(port, () => {
-     console.log(`Server running on port ${port}`)
-});
+app.listen(port, () => console.log("Server Running on port: " + port));
